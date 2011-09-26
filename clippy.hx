ddc79b38217459916e7377d493b14d1489f80c8c
Scott Taylor <scott@railsnewbie.com>
2011-09-26 17:24:57 -0400

    whitespace

--- clippy.hx
+++ clippy.hx
@@ -6,55 +6,55 @@ import flash.text.TextFieldAutoSize;
 import flash.text.TextFormat;
 
 class ButtonUp extends MovieClip { public function new() { super(); } }
-    
+
 class ButtonOver extends MovieClip { public function new() { super(); } }
-    
+
 class ButtonDown extends MovieClip { public function new() { super(); } }
 
 class Clippy {
   // Main
   static function main() {
     var text:String = flash.Lib.current.loaderInfo.parameters.text;
-    
+
     // label
-    
+
     var label:TextField = new TextField();
     var format:TextFormat = new TextFormat("Arial", 10);
-    
+
     label.text = "copy to clipboard";
     label.setTextFormat(format);
     label.textColor = 0x888888;
     label.selectable = false;
     label.x = 15;
     label.visible = false;
-    
+
     flash.Lib.current.addChild(label);
-    
+
     // button
-    
+
     var button:SimpleButton = new SimpleButton();
     button.useHandCursor = true;
     button.upState = flash.Lib.attach("ButtonUp");
     button.overState = flash.Lib.attach("ButtonOver");
     button.downState = flash.Lib.attach("ButtonDown");
     button.hitTestState = flash.Lib.attach("ButtonDown");
-    
+
     button.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
       flash.system.System.setClipboard(text);
       label.text = "copied!";
       label.setTextFormat(format);
     });
-    
+
     button.addEventListener(MouseEvent.MOUSE_OVER, function(e:MouseEvent) {
       label.visible = true;
     });
-    
+
     button.addEventListener(MouseEvent.MOUSE_OUT, function(e:MouseEvent) {
       label.visible = false;
       label.text = "copy to clipboard";
       label.setTextFormat(format);
     });
-    
+
     flash.Lib.current.addChild(button);
   }
 }
