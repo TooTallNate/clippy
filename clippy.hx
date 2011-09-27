c5663c5e310fa5662f4227c4aa486a7d4ea410d1
Scott Taylor <scott@railsnewbie.com>
2011-09-27 11:36:24 -0400

    Remove the label, set the swf to 30x30 for the new assets

--- clippy.hx
+++ clippy.hx
@@ -1,9 +1,6 @@
 import flash.display.MovieClip;
 import flash.events.MouseEvent;
 import flash.display.SimpleButton;
-import flash.text.TextField;
-import flash.text.TextFieldAutoSize;
-import flash.text.TextFormat;
 
 class ButtonUp extends MovieClip { public function new() { super(); } }
 
@@ -14,28 +11,11 @@ class ButtonDown extends MovieClip { public function new() { super(); } }
 class Clippy {
   // Main
   static function main() {
-    var text:String         = flash.Lib.current.loaderInfo.parameters.text;
-    var labelText:String    = flash.Lib.current.loaderInfo.parameters.labelText;
-    var copiedText:String   = flash.Lib.current.loaderInfo.parameters.copiedText;
-
-    // label
-
-    var label:TextField = new TextField();
-    var format:TextFormat = new TextFormat("Arial", 10);
-
-    label.text = labelText;
-    label.setTextFormat(format);
-    label.textColor = 0x888888;
-    label.selectable = false;
-    label.x = 15;
-    label.visible = false;
-
-    flash.Lib.current.addChild(label);
+    var text:String = flash.Lib.current.loaderInfo.parameters.text;
 
     // button
-
     var button:SimpleButton = new SimpleButton();
-    button.useHandCursor = true;
+    button.useHandCursor = false;
     button.upState = flash.Lib.attach("ButtonUp");
     button.overState = flash.Lib.attach("ButtonOver");
     button.downState = flash.Lib.attach("ButtonDown");
@@ -43,18 +23,6 @@ class Clippy {
 
     button.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
       flash.system.System.setClipboard(text);
-      label.text = copiedText;
-      label.setTextFormat(format);
-    });
-
-    button.addEventListener(MouseEvent.MOUSE_OVER, function(e:MouseEvent) {
-      label.visible = true;
-    });
-
-    button.addEventListener(MouseEvent.MOUSE_OUT, function(e:MouseEvent) {
-      label.visible = false;
-      label.text = labelText;
-      label.setTextFormat(format);
     });
 
     flash.Lib.current.addChild(button);
